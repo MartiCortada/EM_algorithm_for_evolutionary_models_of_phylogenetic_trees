6e1272ae29446e4136d889fc9f449a876f759cab
Martí Cortada Garcia <marticortadagarcia@gmail.com>
2023-05-28 08:40:24 +0200

    Changes in the newick format

--- run.sh
+++ run.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
 # Command 1
-echo "running EM_a30_b30_1000_random:"
-python3 EM.py newick3.txt random 1000 100 EM_a30_b30_1000_random
+echo "running EM_a60_b60_500_random:"
+python3 EM.py newick.txt random 500 100 EM_a60_b60_500_random
 
 # Command 2
-echo "running EM_a30_b30_10000_random:"
-python3 EM.py newick3.txt random 10000 100 EM_a30_b30_10000_random
-
-# Command 1
-echo "running EM_a45_b45_500_random:"
-python3 EM.py newick.txt random 500 100 EM_a45_b45_500_random
-
-# Command 1
-echo "running EM_a45_b45_1000_random:"
-python3 EM.py newick.txt random 1000 100 EM_a45_b45_1000_random
-
-# Command 1
-echo "running EM_a45_b45_10000_random:"
-python3 EM.py newick.txt random 10000 100 EM_a45_b45_10000_random
+echo "running EM_a60_b60_1000_random:"
+python3 EM.py newick.txt random 1000 100 EM_a60_b60_1000_random
 
 # Add more commands as needed
 echo "All commands executed."
