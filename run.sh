c051b653781b47bab80b1bb015033296bf1ba083
Martí Cortada Garcia <marticortadagarcia@gmail.com>
2023-05-29 10:33:27 +0200

    Loading new experiments

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Command 1
-echo "running EM_a60_b60_500_random:"
-python3 EM.py newick.txt random 500 100 EM_a60_b60_500_random
+echo "running EM_a75_b75_1000_random:"
+python3 EM.py newick2.txt random 1000 100 EM_a75_b75_1000_random
 
-# Command 2
+# Command 1
 echo "running EM_a60_b60_1000_random:"
 python3 EM.py newick.txt random 1000 100 EM_a60_b60_1000_random
 
