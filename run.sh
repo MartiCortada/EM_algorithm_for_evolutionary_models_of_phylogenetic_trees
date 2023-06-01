be2ec6eac2fe106523ef9edbc8b73e06146812c7
Martí Cortada Garcia <marticortadagarcia@gmail.com>
2023-06-01 13:08:28 +0200

    Loading more experiments a!=b

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 # Command 1
-echo "running EM_a60_b60_10000_random:"
-python3 EM.py newick.txt random 10000 100 EM_a60_b60_10000_random
+echo "running EM_a75_b1_10000_random:"
+python3 EM.py newick.txt random 10000 100 EM_a75_b1_10000_random
+
+# Command 2
+echo "running EM_a1_b75_500_random:"
+python3 EM.py newick2.txt random 500 100 EM_a1_b75_500_random
+
+# Command 3
+echo "running EM_a1_b75_1000_random:"
+python3 EM.py newick2.txt random 1000 100 EM_a1_b75_1000_random
+
+# Command 4
+echo "running EM_a1_b75_10000_random:"
+python3 EM.py newick2.txt random 10000 100 EM_a1_b75_10000_random
 
 # Add more commands as needed
 echo "All commands executed."
